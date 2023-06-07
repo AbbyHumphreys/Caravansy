4a6462fa834a12f03eba23bc370fdf543613ee79
Abby Humphreys <bibby3316@gmail.com>
2023-06-07 21:13:30 +0000

    delete: modal for flash messages

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sellerPhoneButton = document.getElementById('seller-phone-button')
-    const sellerEmailButton = document.getElementById('seller-email-button')
-    const sellerPhoneNumber = document.getElementById('seller-phone-number')
-    const sellerEmail = document.getElementById('seller-email')
-    
+    const sellerPhoneButton = document.getElementById('seller-phone-button');
+    const sellerEmailButton = document.getElementById('seller-email-button');
+    const sellerPhoneNumber = document.getElementById('seller-phone-number');
+    const sellerEmail = document.getElementById('seller-email');
+
     sellerPhoneButton.addEventListener('click', e => {
         displayPhoneNumber();
     });
